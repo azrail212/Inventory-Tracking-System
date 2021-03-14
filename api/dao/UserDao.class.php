5fced0fd34a59b3cbadedef97af7df6791374b6d
azrail212 <becirovicazra00@gmail.com>
2021-03-15 00:19:51 +0100

    Made addUser and getUser classes work with no issue

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -1,29 +1,27 @@
 <?php
-require_once dirname(__FILE__)."/MainDao.class.php";
+require_once dirname(__FILE__)."/BaseDao.class.php";
 
-class UserDao extends MainDao{
+class UserDao extends BaseDao{
 
     public function getUserByID($userID){
         return $this->queryUnique("SELECT * FROM Users WHERE userID= :id", ["id"=>$userID]);
     }
 
     public function getUserByName($name){
-        return $this->queryUnique("SELECT * FROM Users WHERE userName=:name". ["userName"=>$name]);
+        return $this->queryUnique("SELECT * FROM Users WHERE userName=:userName", ["userName"=>$name]);
     }
 
     public function addUser($user){
-        $query = "INSERT INTO Users (userName, userPassword, userStatus, userPermissions) VALUES (:userName, :userPassword, :userStatus, :userPermissions)";
+        $query = "INSERT INTO Users (userName, userPassword, userActive, userPermissions) VALUES (:userName, :userPassword, :userActive, :userPermissions)";
         
         //connection to server, execution of sql
         $stmt = $this->connection->prepare($query);
         $stmt->execute($user);
-        $user['id']= $this->connection->lastInsertId();
-        return $user;
     }
 
     public function changeUserCredentials($userName, $userPassword){
 
-        $query= "UPDATE Users SET userName = :name, userPassword=:password WHERE userName= :name";
+        $query= "UPDATE Users SET userPassword=:password WHERE userName= :name";
         $stmt = $this->connection->prepare($query);
         $userName['name']= $userName;
         $stmt->execute($userName);
