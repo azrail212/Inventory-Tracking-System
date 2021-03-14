1dfec77649cbfb84d69fb9a129d0e95520bcb592
azrail212 <becirovicazra00@gmail.com>
2021-03-15 00:22:05 +0100

    deleted:    api/dao/MainDao.class.php 	modified:   api/index.php

--- api/index.php
+++ api/index.php
@@ -3,13 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once dirname(__FILE__)."/dao/MainDao.class.php";
+require_once dirname(__FILE__)."/dao/BaseDao.class.php";
 require_once dirname(__FILE__)."/dao/UsersDao.class.php";
 
-$mainDaoSetup = new MainDao();
+$baseDaoInit = new BaseDao();
 echo "Hello from API";
-$myUser;
-echo $myUser->getUserByID();
 ?>
 
 
