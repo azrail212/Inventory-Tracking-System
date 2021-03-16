4aaba1e689e013a4052cdef9f502429a9e271fd7
azrail212 <becirovicazra00@gmail.com>
2021-03-16 21:05:22 +0100

    optimized for use with basedao protected functions, debugged getbyname and updatebyname

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -3,33 +3,17 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class UserDao extends BaseDao{
 
-    public function getUserByID($userID){
-        return $this->queryUnique("SELECT * FROM Users WHERE userID= :id", ["id"=>$userID]);
+    public function __construct(){
+        parent::__construct("Users");
     }
 
     public function getUserByName($name){
         return $this->queryUnique("SELECT * FROM Users WHERE userName=:userName", ["userName"=>$name]);
     }
-    
-    //when adding user to db, this will add to db and present him back to us
-    public function addUser($user){
-        $query = "INSERT INTO Users (userName, userPassword, userStatus, userPermissions, userBranchOfficeID) VALUES (:userName, :userPassword, :userStatus, :userPermissions, :userBranchOfficeID)";
-        //connection to server, execution of sql
-        $stmt = $this->connection->prepare($query);
-        $stmt->execute($user);
-        $user['userID']=$this->connection->lastInsertId();
-        return $user; 
-    }
-
-    public function updateUser($userID, $user){
-        $this->update('Users', $userID, $user);
-    }
 
     public function updateUserByName($userName, $user){
-        $this->update('Users', $userName, $user, 'userName');
+        $this->executeUpdate('Users', $userName, $user, 'userName');
     }
-
+    
 }
 ?>
-
-  
