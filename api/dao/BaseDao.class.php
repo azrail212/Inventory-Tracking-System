b03740f6e6b3a9b71c2c7b868660f3438901ea2c
azrail212 <becirovicazra00@gmail.com>
2021-03-16 21:03:47 +0100

    added mutual functions for add,insert,query,queryunique,get, etc

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -2,34 +2,34 @@
 require_once dirname(__FILE__)."/../config.php";
 
 class BaseDao {
+
   protected $connection;
-  public $table;
+  private $table;
+
   //connecting to server
-  public function __construct(){
-    //$this->table=$table;
+  public function __construct($table){
+    $this->table=$table;
+
     try{
       $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      //echo "Connected";
     }
     catch(PDOException $e) {
-    throw $e;
+      throw $e;
     }
   }
 
   //returns a maximum of 25 record from the table currently in focus
-  public function getAll($offset=0, $limit=25){
-    return $this->query("SELECT * FROM".$this->table." LIMIT ${limit} OFFSET {$offset}",[]);
-  }
-
+  
   //inserts wanted values for an entity into a specified table
-  protected function insertIntoTable($table,$entity){
+  protected function insert($table, $entity){
     $query="INSERT INTO ${table} (";
 
     //looping through columns in a row/entity
     foreach($entity as $column => $value){
-      $query .=$column.",";
+      $query .= $column.", ";
     }
+
     $query = substr($query, 0, -2);
     $query.=") VALUES (";
 
@@ -37,23 +37,25 @@ class BaseDao {
     foreach($entity as $column=>$value){
       $query.=":".$column.", ";
     }
+
     $query = substr($query, 0, -2);
     $query.=")";//closing query
 
     //prepare connection to execute $query
     $stmt=$this->connection->prepare($query);
     $stmt->execute($entity); //execution and injection prevention
-    $entity['id']=$this->connection->lastInsertID();//returns ID of the last inserted row or sequence value
+    $entity['id']=$this->connection->lastInsertID(); //returns ID of the last inserted row or sequence value
     return $entity;
   }
   
   //allows changing existing values in any table
-  protected function update($table, $id, $entity, $idColumn="id"){
+  protected function executeUpdate($table, $id, $entity, $idColumn='id'){
     $query= "UPDATE ${table} SET ";
 
     foreach($entity as $name=>$value){
       $query.= $name ."= :". $name. ", ";
     }
+
     $query=substr($query, 0, -2);
     $query .= " WHERE ${idColumn} = :id";
 
@@ -75,15 +77,19 @@ class BaseDao {
   }
 
   public function add($entity){
-    return $this->insertIntoTable($this->table, $entity);
+    return $this->insert($this->table, $entity);
   }
 
-  public function updateByID($id, $entity){
-    $this->execute_update($this->table, $id, $entity);
+  public function update($id, $entity){
+    $this->executeUpdate($this->table, $id, $entity);
+  }
+
+  public function getAll($offset=0, $limit=25){
+    return $this->query("SELECT * FROM ".$this->table." LIMIT ${limit} OFFSET {$offset}",[]);
   }
 
   public function getByID($id){
-    return $this->queryUnique("SELECT * FROM " .$this->table."WHERE id = :id", ["id"=>$id]);
+    return $this->queryUnique("SELECT * FROM " .$this->table." WHERE id = :id", ["id"=>$id]);
   }
 }
 ?>
