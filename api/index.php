a4e398868d0dc3cc84b2898c4ba2aa9cdf120a81
azrail212 <becirovicazra00@gmail.com>
2021-03-21 23:32:49 +0100

    Added basic flightPHP routes

--- api/index.php
+++ api/index.php
@@ -5,7 +5,32 @@ error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
 require_once dirname(__FILE__)."/dao/UserDao.class.php";
+require dirname(__FILE__)."/../vendor/autoload.php";
 
+Flight::register('userDao','UserDao');
 
-echo "Hello from API";
+Flight::route('GET /users', function(){
+    Flight::json(Flight::userDao()->getAll(0,10));
+});
+
+Flight::route('GET /users/@id', function($id){
+    Flight::json(Flight::userDao()->getByID($id));
+});
+
+Flight::route('POST /users', function(){
+    $request = Flight::request()->data->getData();
+    Flight::json(Flight::userDao()->add($data));
+});
+
+Flight::route('PUT /users/@id', function($id){
+    $request = Flight::request();
+    $data = $request->data->getData();
+    Flight::userDao()->update($id, $data);
+    $user = Flight::userDao()->getByID($id);
+    Flight::json($user);
+    
+});
+
+Flight::start();
+
 ?>
