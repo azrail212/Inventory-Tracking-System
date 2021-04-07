c57fb4f6989d732ea2b34bc618fa214309bd6e32
azrail212 <becirovicazra00@gmail.com>
2021-04-07 19:06:45 +0200

    almost deleted project with git, fixing what i can

---
+++ api/dao/BaseDao.class.php
@@ -0,0 +1,95 @@
+<?php
+require_once dirname(__FILE__)."/../services/config.php";
+
+class BaseDao {
+
+  protected $connection;
+  private $table;
+
+  //connecting to server
+  public function __construct($table){
+    $this->table=$table;
+
+    try{
+      $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+      throw $e;
+    }
+  }
+
+  //returns a maximum of 25 record from the table currently in focus
+  
+  //inserts wanted values for an entity into a specified table
+  protected function insert($table, $entity){
+    $query="INSERT INTO ${table} (";
+
+    //looping through columns in a row/entity
+    foreach($entity as $column => $value){
+      $query .= $column.", ";
+    }
+
+    $query = substr($query, 0, -2);
+    $query.=") VALUES (";
+
+    //looping again, appending values to the query 
+    foreach($entity as $column=>$value){
+      $query.=":".$column.", ";
+    }
+
+    $query = substr($query, 0, -2);
+    $query.=")";//closing query
+
+    //prepare connection to execute $query
+    $stmt=$this->connection->prepare($query);
+    $stmt->execute($entity); //execution and injection prevention
+    $entity['id']=$this->connection->lastInsertID(); //returns ID of the last inserted row or sequence value
+    return $entity;
+  }
+  
+  //allows changing existing values in any table
+  protected function executeUpdate($table, $id, $entity, $idColumn='id'){
+    $query= "UPDATE ${table} SET ";
+
+    foreach($entity as $name=>$value){
+      $query.= $name ."= :". $name. ", ";
+    }
+
+    $query=substr($query, 0, -2);
+    $query .= " WHERE ${idColumn} = :id";
+
+    $stmt = $this->connection->prepare($query);
+    $entity['id']=$id;
+    $stmt->execute($entity);
+  }
+
+  //this can execute any kind of query on the database
+  protected function query($query, $parameters){
+    $stmt = $this->connection->prepare($query);
+    $stmt->execute($parameters); //executes query using parameters we passed
+    return $stmt->fetchAll(PDO::FETCH_ASSOC); //returns an array indexed by column name as returned in your result set
+  }
+
+  protected function queryUnique($query, $parameters){
+    $results = $this->query($query, $parameters);
+    return reset($results);
+  }
+
+  public function add($entity){
+    return $this->insert($this->table, $entity);
+  }
+
+  public function update($id, $entity){
+    $this->executeUpdate($this->table, $id, $entity);
+  }
+
+  public function getAll($offset=0, $limit=25){
+    return $this->query("SELECT * FROM ".$this->table." LIMIT ${limit} OFFSET ${offset}",[]);
+  }
+
+  public function getByID($id){
+    return $this->queryUnique("SELECT * FROM " .$this->table." WHERE id = :id", ["id"=>$id]);
+  }
+}
+?>
---
+++ api/dao/UserDao.class.php
@@ -0,0 +1,25 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class UserDao extends BaseDao{
+
+    public function __construct(){
+        parent::__construct("Users");
+    }
+
+    public function getUserByName($name){
+        return $this->queryUnique("SELECT * FROM Users WHERE userName=:userName", ["userName"=>$name]);
+    }
+
+    public function updateUserByName($userName, $user){
+        $this->executeUpdate('Users', $userName, $user, 'userName');
+    }
+
+    public function getUsersBySearch($search, $offset, $limit){
+        return $this->query("SELECT * FROM Users 
+            WHERE LOWER(userName) LIKE CONCAT('%', :name , '%') 
+                LIMIT ${limit} OFFSET ${offset}", ["name" => strtolower($search)]);
+    }
+    
+}
+?>
